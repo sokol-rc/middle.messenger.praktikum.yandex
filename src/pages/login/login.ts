f57fd5cf3df05a77f8ad75d23257778993a25ce8
Evgeniy Sokolovskiy <cokol-rc@yandex.ru>
2022-09-28 03:26:51 +0300

    validate in process

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,19 +1,34 @@
 /* eslint-disable */
 import Block from 'core/Block';
+import { formValidate, showErrors, showSuccess } from 'utils/validate';
 
 export class LoginPage extends Block {
-	constructor() { 
-		super();
+    constructor() {
+        super();
 
 		this.setProps({
-			handleClick: (event: MouseEvent): void => { 
-				event.preventDefault();
-				const isValid: boolean = validate(form);
-				console.log(isValid);
-				console.log(event)
-			}
-		})
+			handleClick: this.handleClick.bind(this),
+        });
 	}
+	handleClick(event: MouseEvent): void {
+		event.preventDefault();
+		const formElement = document.querySelector(
+			'.auth-form'
+		) as HTMLFormElement;
+		const validation = formValidate(formElement);
+
+		if (
+			validation.error !== 0 &&
+			typeof validation.elements !== 'undefined'
+		) {
+			console.log('Не валидный');
+			showErrors(formElement, validation.elements);
+		} else { 
+			showSuccess(formElement);
+		}
+	}
+	
+
     render() {
         return `
 		<main class="auth-content layout-container">
@@ -24,16 +39,20 @@ export class LoginPage extends Block {
 		<form class="auth-form form form-container" action="">
 			<div class="form-input">
 				<label class="form-input__label" for="authFormEmail">Email</label>
-				<input class="form-input__input" id="authFormEmail" type="email" placeholder="your-email@mail.com" name="login">
+				<input class="form-input__input" id="authFormEmail" type="email" placeholder="your-email@mail.com" name="login" data-validate="login">
+				<div class="error-message"></div>
 			</div>
 			<div class="form-input">
 				<label class="form-input__label" for="authFormPassword">Пароль</label>
-				<input class="form-input__input" id="authFormPassword" type="password" placeholder="пароль" name="password">
+				<input class="form-input__input" id="authFormPassword" type="password" placeholder="пароль" name="password" minlength="8" maxlength="50" required>
+				<div class="error-message"></div>
 			</div>
 			<div class="form__check policy-check">
-				<input class="policy-check__input" id="authFormPolicy" type="checkbox">
+				<input class="policy-check__input" id="authFormPolicy" type="checkbox" required>
+				
 				<label class="policy-check__label" for="authFormPolicy">Оставляя свой email, я принимаю <a href=""
 						class="policy-check__link link link--standart">политику конфиденциальности</a></label>
+						<div class="error-message"></div>
 			</div>
 			<div class="form__control form-btns">
 				<div class="form-btns__forgot-password">
